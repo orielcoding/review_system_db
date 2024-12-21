9eedd61ae121a87a1412ac77022f9a1aa8e17e5c
Oriel Singer <oriel2486@gmail.com>
2024-12-21 20:28:15 +0200

    Separation of concerns: all db interactions passed as methods to the relevant script. main is only calling them.
    adding todo notes to pass validations from main to app.

--- db_interactions.py
+++ db_interactions.py
@@ -1,4 +1,62 @@
-from main import cursor
+import json
+import mysql.connector
+
+with open('config.json') as f:
+    config = json.load(f)
+
+connection = mysql.connector.connect(  # TODO: separate concerns - all db actions in db_interactions.py
+    host=config['host'],
+    user=config['user'],
+    password=config['password'],
+    database=config['database']
+)
+cursor = connection.cursor()
+
+
+def add_row_reviews(request):
+    cursor.execute("""
+    INSERT INTO reviews (review_content, review_rating, store_id, product_id, request_time)
+    VALUES (%s, %s, %s, %s, %s)
+    """, request)
+
+
+def update_reviews_table(request_time, review_content, review_id, review_rating):
+    cursor.execute("""
+    UPDATE reviews
+    SET review_content = %s, review_rating = %s, request_time = %s
+    WHERE review_id = %s
+    """, (review_content, review_rating, request_time, review_id))
+
+
+def delete_from_stores_reviews(review_id):
+    cursor.execute("""
+    DELETE FROM stores_reviews
+    WHERE review_id = %s
+    """, (review_id,))
+
+
+def delete_from_reviews(review_id):
+    cursor.execute("""
+    DELETE FROM reviews
+    WHERE review_id = %s
+    """, (review_id,))
+
+
+def add_row_stores_reviews(review_id, store_id):
+    cursor.execute("""
+    INSERT INTO stores_reviews (review_id, store_id)
+    VALUES (%s, %s)
+    """, (review_id, store_id))
+
+
+def review_existence_check(review_id):
+    cursor.execute("""
+    SELECT review_id
+    FROM reviews
+    WHERE review_id = %s
+    """, (review_id,))
+    if not cursor.fetchone():
+        raise ValueError('Review id does not exist')
 
 
 def checkStoreId(source_store_id):
@@ -7,3 +65,40 @@ def checkStoreId(source_store_id):
     FROM stores
     WHERE store_id = %s
     """, (source_store_id,))
+    if not cursor.fetchone():
+        raise ValueError('Source store id does not exist')
+
+
+def get_store_organization(source_store_id):
+    cursor.execute("""
+    SELECT organization_token
+    FROM stores
+    WHERE store_id = %s
+    """, (source_store_id,))
+    source_store_organization = cursor.fetchone()
+    return source_store_organization
+
+
+def add_row_syndicate(source_store_id, target_store_id):
+    cursor.execute("""
+    INSERT INTO syndicate (source_store_id, target_store_id)
+    VALUES (%s, %s)
+    """, (source_store_id, target_store_id))
+
+
+def get_source_stores(store_id):
+    cursor.execute("""
+    SELECT source_store_id
+    FROM syndicate
+    WHERE target_store_id = %s
+    """, (store_id,))
+    source_stores_id = cursor.fetchall()
+    return source_stores_id
+
+
+def get_all_reviews(store_id):
+    cursor.execute("""
+    SELECT review_content
+    FROM reviews
+    WHERE store_id = %s
+    """, store_id)
--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-import datetime
-import mysql.connector
-import json
 import logging
-from DTOs_definitions import CreateReviewRequest, UpdateReviewRequest
-from db_interactions import checkStoreId
+from DTOs_definitions import *
+from db_interactions import *
 
-with open('config.json') as f:
-    config = json.load(f)
 
-connection = mysql.connector.connect(  # TODO: separate concerns - all db actions in db_interactions.py
-    host=config['host'],
-    user=config['user'],
-    password=config['password'],
-    database=config['database']
-)
-cursor = connection.cursor()
-
-# create logging file for create, update, delete and get functions altogether.
-logging.basicConfig(filename=f'review_reports.log', level=logging.INFO)  # can be further improved
+# create a single logging file. It can be further improved to separate logs for each function / db table / client.
+logging.basicConfig(filename=f'review_reports.log', level=logging.INFO)
 
 
 def create_review(request: CreateReviewRequest) -> int | None:
@@ -28,7 +15,7 @@ def create_review(request: CreateReviewRequest) -> int | None:
     """
     review_content, review_rating, store_id, product_id, request_time = request
 
-    def validate_create_request() -> bool:
+    def validate_create_request() -> bool:  # TODO: separate concerns to app after merging, the rules should remain here
         if not type(review_content) == str or not len(review_content) <= 100:
             logging.info(f'Attempted to create review with invalid content: {review_content}')
             print('Review content must be a string with less than 100 characters')
@@ -54,20 +41,15 @@ def create_review(request: CreateReviewRequest) -> int | None:
         return
 
     # save to reviews table
-    cursor.execute("""
-    INSERT INTO reviews (review_content, review_rating, store_id, product_id, request_time)
-    VALUES (%s, %s, %s, %s, %s)
-    """, request)
-
+    add_row_reviews(request)
     logging.info(
         f'Created review with content: {review_content}, rating: {review_rating}, store id: {store_id}, product id: {product_id}, request time: {request_time}')
 
     review_id = cursor.lastrowid
+
     # save to stores_reviews table with generated id from mysql
-    cursor.execute("""
-    INSERT INTO stores_reviews (review_id, store_id)
-    VALUES (%s, %s)
-    """, (review_id, store_id))
+    add_row_stores_reviews(review_id, store_id)
+    logging.info(f'Created review with id: {review_id} in stores_reviews table.')
 
     return review_id
 
@@ -82,15 +64,13 @@ def update_review(request: UpdateReviewRequest) -> bool:
     review_id, review_content, review_rating, request_time = request
 
     # check if review_id exists
-    cursor.execute("""
-    SELECT review_id
-    FROM reviews
-    WHERE review_id = %s
-    """, (review_id,))
-    if not cursor.fetchone():
-        raise ValueError('Review id does not exist')
+    try:
+        review_existence_check(review_id)
+    except ValueError as e:
+        print(e)
+        return False
 
-    def validate_update_request() -> bool:
+    def validate_update_request() -> bool:  # TODO: separate concerns to app after merging, the rules should remain here
         if not type(review_id) == int:
             print('Review id must be an integer')
             return False
@@ -112,11 +92,7 @@ def update_review(request: UpdateReviewRequest) -> bool:
     logging.info(f'Old review data: {cursor.fetchone()}')
 
     # update reviews table
-    cursor.execute("""
-    UPDATE reviews
-    SET review_content = %s, review_rating = %s, request_time = %s
-    WHERE review_id = %s
-    """, (review_content, review_rating, request_time, review_id))
+    update_reviews_table(request_time, review_content, review_id, review_rating)
 
     # log the new data
     logging.info(f'New data: {cursor.fetchone()}')
@@ -131,36 +107,24 @@ def delete_review(review_id: int) -> bool:
     """
 
     # check if review_id exists
-    cursor.execute("""
-    SELECT review_id
-    FROM reviews
-    WHERE review_id = %s
-    """, (review_id,))
-    if not cursor.fetchone():
-        logging.info(f'Attempted to delete review with non existing id: {review_id} and failed.')
-        print('Review id does not exist')
+    try:
+        review_existence_check(review_id)
+    except ValueError as e:
+        print(e)
         return False
 
     # delete from reviews table
-    cursor.execute("""
-    DELETE FROM reviews
-    WHERE review_id = %s
-    """, (review_id,))
-
+    delete_from_reviews(review_id)
     logging.info(f'Deleted review with id: {review_id} from reviews table.')
 
     # delete from stores_reviews table
-    cursor.execute("""
-    DELETE FROM stores_reviews
-    WHERE review_id = %s
-    """, (review_id,))
-
+    delete_from_stores_reviews(review_id)
     logging.info(f'Deleted review with id: {review_id} from stores_reviews table.')
 
     return True
 
 
-def syndicate_stores(source_store_id: int, target_store_id: int) -> bool:  # TODO: correct the function - understand the requirements
+def syndicate_stores(source_store_id: int, target_store_id: int) -> bool:
     """
     Syndicate reviews from source_store_id to target_store_id according to syndication rules.
     The target store is the client's store. The source store is the store that the client
@@ -168,33 +132,24 @@ def syndicate_stores(source_store_id: int, target_store_id: int) -> bool:  # TOD
     """
 
     # check if source_store_id exists
-    checkStoreId(source_store_id)
-    if not cursor.fetchone():
-        logging.info(f'Attempted to syndicate reviews from non existing source store id: {source_store_id} and failed.')
+    try:
+        checkStoreId(source_store_id)
+    except ValueError as e:
         print('Source store id does not exist')
+        logging.info(f'Attempted to syndicate reviews from non existing source store id: {source_store_id} and failed.')
         return False
 
     # check if target_store_id exists
-    checkStoreId(target_store_id)
-    if not cursor.fetchone():
-        logging.info(f'Attempted to syndicate reviews to non existing target store id: {target_store_id} and failed.')
-        print('Target store id does not exist')
+    try:
+        checkStoreId(target_store_id)
+    except ValueError as e:
+        print('Source store id does not exist')
+        logging.info(f'Attempted to syndicate reviews from non existing source store id: {source_store_id} and failed.')
         return False
 
     # check source and target store organization
-    cursor.execute("""
-    SELECT organization_token
-    FROM stores
-    WHERE store_id = %s
-    """, (source_store_id,))
-    source_store_organization = cursor.fetchone()
-
-    cursor.execute("""
-    SELECT organization_token
-    FROM stores
-    WHERE store_id = %s
-    """, (target_store_id,))
-    target_store_organization = cursor.fetchone()
+    source_store_organization = get_store_organization(source_store_id)
+    target_store_organization = get_store_organization(target_store_id)
 
     # check if source and target store belong to the same organization
     if source_store_organization != target_store_organization:
@@ -203,10 +158,7 @@ def syndicate_stores(source_store_id: int, target_store_id: int) -> bool:  # TOD
         return False
 
     # add row in syndicate table
-    cursor.execute("""
-    INSERT INTO syndicate (source_store_id, target_store_id)
-    VALUES (%s, %s)
-    """, (source_store_id, target_store_id))
+    add_row_syndicate(source_store_id, target_store_id)
 
     logging.info(f'Successfully syndicated reviews from source store id: {source_store_id} to target store id: {target_store_id}.')
     return True
@@ -219,27 +171,13 @@ def get_reviews(store_id: int) -> list:
     :param store_id - the target store from which a client want to extract reviews.
     """
 
-    # examine clients' token to verify the store he belongs to.
-    if not credentials_check(target_store_id, token):  # TODO: separate concerns
-        print('stores are not in the same organization')
-        return []
-
     # check all source stores that syndicate to the target store
-    cursor.execute("""
-    SELECT source_store_id
-    FROM syndicate
-    WHERE target_store_id = %s
-    """, (store_id,))
-    source_stores_id = cursor.fetchall()
+    source_stores_id = get_source_stores(store_id)
 
     # get reviews contents alone from the target store - not including any database access
     reviews = []
-    # get reviuews from target store
-    cursor.execute("""
-    SELECT review_content
-    FROM reviews
-    WHERE store_id = %s
-    """, store_id)
+    # get reviews from target store
+    get_all_reviews(store_id)
     rows = cursor.fetchall()
     # the rows are tuples. add the data to reviews as json string
     reviews.extend([row[0] for row in rows])
@@ -249,11 +187,7 @@ def get_reviews(store_id: int) -> list:
     # get reviews from source stores
     for source_store_id in source_stores_id:
         syndicate_stores(source_store_id, store_id)
-        cursor.execute("""
-        SELECT review_content
-        FROM reviews
-        WHERE store_id = %s
-        """, source_store_id)
+        get_all_reviews(source_store_id)
 
         logging.info(f'Getting reviews from source store id: {source_store_id}')  # here I can add more advanced logging
 
@@ -261,11 +195,3 @@ def get_reviews(store_id: int) -> list:
         reviews.extend([row[0] for row in rows])
 
     return reviews
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
